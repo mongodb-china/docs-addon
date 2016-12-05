ec4f29cceee4323983a83e59a7336fbdcdb5dce5
TJ <evertang@gmail.com>
2016-12-05 18:07:13 +0800

    update

--- docker/etc/setup.sh
+++ docker/etc/setup.sh
@@ -1,26 +1,16 @@
 #!/bin/sh
 
+cd /opt/docs-addon 
+git pull origin master
 
-
-# checkout source codes
-echo "cloning source"
-git clone http://github.com/mongodb-china/docs.git /opt/docs
-git clone http://github.com/mongodb-china/docs-addon.git /opt/docs-addon
-
-# create symbol link for web
-mkdir -p /opt/docs/build/master/html-zh
-ln -sf /opt/docs/build/master/html-zh /var/www/html/manual-zh
-/etc/init.d/apache2 restart
-
-# start github-webhook
-echo "Stating github-webhook 8001"
-cd /opt/docs-addon/github-hook
-nohup python index.py 8001 > hook.log &
+cd /opt/docs
+git pull origin master
 
 # start direct push script
 echo "Starting direct push script 8080"
 cd /opt/docs
 
-# edit index.py/repos.json 
-# start flask daemon on port 8081
-
+# start github-webhook
+echo "Stating github-webhook 8001"
+cd /opt/docs-addon/github-hook
+python index.py 8001 
