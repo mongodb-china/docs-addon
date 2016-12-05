d14308e8275dd5218cc0909a0c674ead00a6ad37
TJ <evertang@gmail.com>
2016-12-05 23:34:23 +0800

    add theme file

--- docker/etc/setup.sh
+++ docker/etc/setup.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
 
-cd /opt/docs-addon 
+if [ -d "/opt/docs/.git" ]; then
+	# mounted host folder
+	cd /opt/docs 
+	git pull origin master
+else
+	#empty
+	mkdir -p /opt/docs/build
+	git clone http://github.com/mongodb-china/docs.git /opt/docs
+	git clone http://github.com/mongodb/docs-tools.git /opt/docs/build/docs-tool
+fi
+
+cd /opt/docs-addon
 git pull origin master
 
-cd /opt/docs
-git pull origin master
+# create shortcut for manual file 
+mkdir -p /opt/docs/build/master/html-zh
+ln -sf /opt/docs/build/master/html-zh /var/www/html/manual-zh
+
+# user manual-cn theme
+sed -i "s/name: 'manual'/name: 'manual-cn'/"  /opt/docs/config/sphinx_local.yaml  
 
 # start direct push script
 echo "Starting direct push script 8080"
-cd /opt/docs
+echo "TBD"
 
 # start github-webhook
 echo "Stating github-webhook 8001"
 cd /opt/docs-addon/github-hook
-python index.py 8001 
+python index.py 8080 
