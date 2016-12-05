57fa8c1e8ae3bad198fa0abab4a072868646608b
TJ <evertang@gmail.com>
2016-12-05 17:28:47 +0800

    update docker file

--- docker/etc/setup.sh
+++ docker/etc/setup.sh
@@ -1,2 +1,18 @@
-git clone http://github.com/tjworks/docs.git /opt/mongo-docs
-python bootstrap.py
+#!/bin/sh
+
+#/etc/init.d/apache2 restart
+
+# checkout source codes
+git clone http://github.com/mongodb-china/docs.git /opt/docs
+git clone http://github.com/mongodb-china/docs-addon.git /opt/docs-addon
+
+# start github-webhook
+cd /opt/docs-addon/github-hook
+nohup python index.py 8001 > hook.log &
+
+# start direct push script
+cd /opt/docs
+
+# edit index.py/repos.json 
+# start flask daemon on port 8081
+
